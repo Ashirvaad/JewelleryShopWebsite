db5cba0596f35d572aa5bb77f967a0ff831e094e
Ashirvaad <68454045+Ashirvaad@users.noreply.github.com>
2025-10-15 17:36:12 +0530

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,119 @@
+// Sample product data
+const products = [
+  {
+    id: 1,
+    name: "Bridal Necklace",
+    price: "₹125,000",
+    image: require("../assets/products/bridal.jpg"),
+  },
+  {
+    id: 2,
+    name: "Diamond Bangles",
+    price: "₹85,000",
+    image: require("../assets/products/diamondBangle.jpg"),
+  },
+  {
+    id: 3,
+    name: "Diamond Ring",
+    price: "₹95,000",
+    image: require("../assets/products/diamond.jpg"),
+  },
+  {
+    id: 4,
+    name: "Diamond Ring",
+    price: "₹110,000",
+    image: require("../assets/products/DiamondRing.jpg"),
+  },
+  {
+    id: 5,
+    name: "Emarald Pendant",
+    price: "₹75,000",
+    image: require("../assets/products/emarald.jpg"),
+  },
+  {
+    id: 6,
+    name: "Emarald Pendant",
+    price: "₹75,000",
+    image: require("../assets/products/emaraldPendant.jpg"),
+  },
+  {
+    id: 7,
+    name: "Emarald Ring",
+    price: "₹75,000",
+    image: require("../assets/products/emaraldRing.jpg"),
+  },
+  {
+    id: 8,
+    name: "Peacock Necklace",
+    price: "₹75,000",
+    image: require("../assets/products/peacockNecklace.jpg"),
+  },
+  {
+    id: 9,
+    name: "Pearl Necklace",
+    price: "₹75,000",
+    image: require("../assets/products/pearl.jpg"),
+  },
+  {
+    id: 10,
+    name: "Pearl Crown",
+    price: "₹75,000",
+    image: require("../assets/products/pearlCrown.jpg"),
+  },
+  {
+    id: 11,
+    name: "Gold Pendant",
+    price: "₹75,000",
+    image: require("../assets/products/pendant.jpg"),
+  },
+  {
+    id: 12,
+    name: "Gold Ring",
+    price: "₹75,000",
+    image: require("../assets/products/ring.jpg"),
+  },
+  {
+    id: 13,
+    name: " Royal Necklace",
+    price: "₹75,000",
+    image: require("../assets/products/royal.jpg"),
+  },
+  {
+    id: 14,
+    name: "Ruby Pendant",
+    price: "₹75,000",
+    image: require("../assets/products/ruby.jpg"),
+  },
+  {
+    id: 15,
+    name: "Gold Necklace",
+    price: "₹75,000",
+    image: require("../assets/products/set1.jpg"),
+  },
+    {
+    id: 16, 
+    name: "Gold Necklace",
+    price: "₹75,000",
+    image: require("../assets/products/set2.jpg"),
+  },
+    {
+    id: 17,
+    name: "Gold Necklace",
+    price: "₹75,000",
+    image: require("../assets/products/set3.jpg"),
+  },
+    {
+    id: 18,
+    name: "Gold Necklace",
+    price: "₹75,000",
+    image: require("../assets/products/set4.jpg"),
+  },
+    {
+    id: 19,
+    name: "Silver Necklace",
+    price: "₹75,000",
+    image: require("../assets/products/silverNecklace.jpg"),
+  },
+];
+
+export default products;
